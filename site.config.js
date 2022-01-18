c2f5ca41fbaf20b0b92fa37cf1778c6165f42876
iisleeping <97940152+iisleeping@users.noreply.github.com>
2022-01-18 16:04:11 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -16,10 +16,7 @@ module.exports = {
   socialImageTitle: 'Transitive Bullshit',
   socialImageSubtitle: 'Hello World! 👋',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+ 
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
