c1193c3e02f0205aa4f0457288804cd265d306d9
iisleeping <97940152+iisleeping@users.noreply.github.com>
2022-01-18 15:35:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ef978e85de3f435abf204355b5d64ff0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '怀明的杂货铺',
+  domain: 'https://www.notion.so/bala-ef978e85de3f435abf204355b5d64ff0',
+  author: '怀明',
 
   // open graph metadata (optional)
   description: 'Example site description',
